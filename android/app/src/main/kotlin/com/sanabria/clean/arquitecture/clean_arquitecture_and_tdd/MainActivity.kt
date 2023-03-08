8ed95270a5335ae06d38142b909ec74961fa4a16
Diego S <dsanabria@deltacontadores.com>
2023-03-08 00:58:10 -0600

    Inicia demo usando Clean Arquitecture
    Crea estructura de directorios para Clean Arquitecture.
    
    Usa TDD para comenzar el caso de uso para obtener un numero en concreto.

---
+++ android/app/src/main/kotlin/com/sanabria/clean/arquitecture/clean_arquitecture_and_tdd/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.sanabria.clean.arquitecture.clean_arquitecture_and_tdd
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ test/features/number_trivia/domain/usecases/get_concret_number_trivia_test.dart
@@ -0,0 +1,36 @@
+import 'package:clean_arquitecture_and_tdd/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:clean_arquitecture_and_tdd/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+import 'package:clean_arquitecture_and_tdd/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
+import 'package:dartz/dartz.dart';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockNumberTriviaRepository extends Mock
+    implements NumberTriviaRepository {}
+
+void main() {
+  late GetConcreteNumberTrivia usecase;
+  late MockNumberTriviaRepository mockNumberTriviaRepository;
+
+  setUp(() {
+    mockNumberTriviaRepository = MockNumberTriviaRepository();
+    usecase = GetConcreteNumberTrivia(mockNumberTriviaRepository);
+  });
+
+  const tNumber = 1;
+  const tNumberTrivia = NumberTrivia(number: tNumber, text: 'test');
+
+  test('should get trivia for the number from the repository', () async {
+    // arrange
+    when(() => mockNumberTriviaRepository.getConcreteNumberTrivia(any()))
+        .thenAnswer((_) async => const Right(tNumberTrivia));
+    // act
+    final result = await usecase(number: tNumber);
+    // assert
+    expect(result, const Right(tNumberTrivia));
+    verify(() => mockNumberTriviaRepository.getConcreteNumberTrivia(tNumber))
+        .called(1);
+    verifyNoMoreInteractions(mockNumberTriviaRepository);
+  });
+}
