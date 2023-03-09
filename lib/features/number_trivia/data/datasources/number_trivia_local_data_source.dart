3a5e9221e052eb2a26b7fdfd366d898c230afe63
Diego S <dsanabria@deltacontadores.com>
2023-03-08 20:42:29 -0600

    Añade contratos para capa de datos
    Añade tests para capa de datos.

---
+++ lib/features/number_trivia/data/datasources/number_trivia_local_data_source.dart
@@ -0,0 +1,11 @@
+import 'package:clean_arquitecture_and_tdd/features/number_trivia/data/models/number_trivia_model.dart';
+
+abstract class NumberTriviaLocalDataSource {
+  /// Gets the cached [NumberTriviaModel] which was gotten the last time
+  /// the user had an interner connection.
+  ///
+  /// Throws a [CacheException] if no cached data is present.
+  Future<NumberTriviaModel> getLastNumberTrivia();
+
+  Future<void> cacheNumberTrivia(NumberTriviaModel triviaToCache);
+}
